ab06f5082cce44f64ea7d012550bd627385f9403
TABET Damien <tabetdamien@free.fr>
2019-05-09 14:21:29 +0200

    Display all articles in homepage

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $articles = ModelFactory::get('Article')->articles();
+        $articles = ModelFactory::get('Article')->getAllArticles();
         echo $this->twig->render('index.html.twig', ['articles' => $articles]);
     }
 }
--- src/Models/Article.php
+++ src/Models/Article.php
@@ -6,5 +6,12 @@ use Core\Model\Model;
 
 class Article extends Model
 {
+    public function getAllArticles()
+    {
+        $query = 'SELECT * FROM `article` a
+                LEFT JOIN `user` u
+                    ON (a.`id_user` = u.`id`)';
+        return $this->database->results($query);
+    }
     
 }
