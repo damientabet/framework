09b85d5a9897dbda7664ee70d3954e4e8a6bf4b8
TABET Damien <tabetdamien@free.fr>
2019-05-27 20:48:40 +0200

    Create URL Processing

--- src/Controllers/FrontController.php
+++ src/Controllers/FrontController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use Core\Router\Router;
+
 class FrontController extends Controller
 {
     private $routes = array(
@@ -26,4 +28,38 @@ class FrontController extends Controller
             'authentification' => array("controller" => "User", "action" => "authentification"),
         )
     );
+
+    public function run()
+    {
+        $router = new Router(isset($_GET['url']) ? $_GET['url'] : '/');
+        $url = explode('/', $_GET['url']);
+
+        switch (count($url)){
+            case '2' :
+                if (is_numeric($url[1])) {
+                    $url = $url[0].'/:id';
+                } elseif (is_string($url[1]) && strstr('-', $url[1])) {
+                    $url = $url[0].'/:id-:slug';
+                } else {
+                    $url = implode('/', $url);
+                }
+                break;
+            case '3' :
+                $url = $url[0].'/'.$url[1].'/:id';
+                break;
+            default :
+                $url = implode('/', $url);
+                break;
+        }
+
+        $route = isset($this->routes[$_SERVER['REQUEST_METHOD']][$url]) ? $this->routes[$_SERVER['REQUEST_METHOD']][$url]: null;
+
+        if ($route != null) {
+            $router->dispatch($url, $route['controller'] . '#' . $route['action'], $_SERVER['REQUEST_METHOD']);
+        } else {
+            $router->dispatch('', 'Index#index', 'GET');
+        }
+
+        $router->run();
+    }
 }
