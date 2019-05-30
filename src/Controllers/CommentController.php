4f94a678bb7c8b683ea2690281e27e15cfaba6ec
TABET Damien <tabetdamien@free.fr>
2019-05-30 14:57:45 +0200

    Add function to add comment

--- src/Controllers/CommentController.php
+++ src/Controllers/CommentController.php
@@ -1,1 +1,26 @@
 <?php
+
+namespace App\Controllers;
+
+use Core\Model\ModelFactory;
+
+class CommentController extends Controller
+{
+    public function addComment($id)
+    {
+        if (!empty($_POST['comment'])) {
+            $data = [
+                'content' => $_POST['comment'],
+                'id_article' => $id,
+                'id_user' => $_SESSION['user']['id'],
+                'date_add' => date('Y-m-d H:i:s')
+            ];
+
+            if (ModelFactory::get('Comment')->create($data)) {
+                return true;
+            } else{
+                $this->errors[] = 'Erreur lors de l\'enregistrement';
+            }
+        }
+    }
+}
