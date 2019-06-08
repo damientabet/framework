b2f366f59d89d0f2997f736bdffb962d227dbf68
TABET Damien <tabetdamien@free.fr>
2019-06-08 11:17:05 +0200

    Replace IndexController to FrontController

--- src/Controllers/front/FrontController.php
+++ src/Controllers/front/FrontController.php
@@ -3,7 +3,56 @@
 namespace App\Controllers\front;
 
 use App\Controllers\Controller;
+use Core\Model\ModelFactory;
 
 class FrontController extends Controller
 {
+    public function index()
+    {
+        $articles = ModelFactory::get('Article')->getAllArticles();
+        echo $this->twig->render('front/index.html.twig', ['articles' => $articles]);
+    }
+
+    public function authentification()
+    {
+        $this->connectionUser();
+        $this->createUser();
+
+        echo $this->twig->render('front/authentification.html.twig',
+            [
+                'errors' => $this->errors
+            ]);
+    }
+
+    public function connectionUser()
+    {
+        if (isset($_POST['connection'])) {
+            $email = $_POST['connection_email'];
+            $user = ModelFactory::get('User')->read($email, 'email');
+            if (empty($_POST['connection_email']) || empty($_POST['connection_password'])) {
+                return $this->errors[] = 'Please fill fields';
+            }
+            if ($user) {
+                if (password_verify($_POST['connection_password'], $user['password'])) {
+                    $_SESSION['user'] = [
+                        'id' => $user['id_user'],
+                        'lastname' => $user['lastname'],
+                        'firstname' => $user['firstname'],
+                        'email' => $email
+                    ];
+                    header('Location: /user/' . $_SESSION['user']['id']);
+                }
+            } else {
+                $this->errors[] = 'No matching user';
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function logout()
+    {
+        session_destroy();
+        header('Location: /');
+    }
 }
