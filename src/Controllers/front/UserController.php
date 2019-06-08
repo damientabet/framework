715773c9c53cd0f5e1f0ad163abdcdb97f0924a1
TABET Damien <tabetdamien@free.fr>
2019-06-08 11:20:38 +0200

    Delete connexion method from UserController.php

--- src/Controllers/front/UserController.php
+++ src/Controllers/front/UserController.php
@@ -18,49 +18,6 @@ class UserController extends FrontController
         );
     }
 
-    public function authentification()
-    {
-        $this->connectionUser();
-        $this->createUser();
-
-        echo $this->twig->render('front/authentification.html.twig',
-            [
-                'errors' => $this->errors
-            ]);
-    }
-
-    public function connectionUser()
-    {
-        if (isset($_POST['connection'])) {
-            $email = $_POST['connection_email'];
-            $user = ModelFactory::get('User')->read($email, 'email');
-            if (empty($_POST['connection_email']) || empty($_POST['connection_password'])) {
-                return $this->errors[] = 'Please fill fields';
-            }
-            if ($user) {
-                if (password_verify($_POST['connection_password'], $user['password'])) {
-                    $_SESSION['user'] = [
-                        'id' => $user['id_user'],
-                        'lastname' => $user['lastname'],
-                        'firstname' => $user['firstname'],
-                        'email' => $email
-                    ];
-                    header('Location: /user/' . $_SESSION['user']['id']);
-                }
-            } else {
-                $this->errors[] = 'No matching user';
-            }
-        } else {
-            return false;
-        }
-    }
-
-    public function logout()
-    {
-        session_destroy();
-        header('Location: /');
-    }
-
     public function createUser()
     {
         if (isset($_POST['addUser'])) {
